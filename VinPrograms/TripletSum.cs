2bd108f6dceaa505856282e16653bc71fbfaeb80
Vinay Kumar <vnetpgmr@gmail.com>
2021-11-30 20:38:00 -0800

    TripletSum

--- VinPrograms/TripletSum.cs
+++ VinPrograms/TripletSum.cs
@@ -32,8 +32,43 @@ namespace VinPrograms
             }
         }
 
+        
+
         /// <summary>
-        /// O(n^2)
+        /// O(n^2), can try. But require to update PairSum class methods.
+        /// </summary>
+        public void PrintTriletSumUsingPairSum()
+        {
+            
+        }
+
+        /// <summary>
+        /// O(n^2), space complexity O(n)
+        /// </summary>
+        public void PrintTriletSumUsingHashSet()
+        {
+            //int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 15 };
+            //int target = 18;
+            int[] arr = { 3,2,1 };
+            int target = 6;
+
+            for (int i = 0; i < arr.Length-2; i++)
+            {
+                HashSet<int> unorderedSet = new HashSet<int>();
+                int targetPairSum = target - arr[i];
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    if(unorderedSet.Contains(targetPairSum-arr[j]))
+                    {
+                        Console.WriteLine($"Sum of triplet {arr[i]}, {arr[j]}, {targetPairSum - arr[j]} is {target}");
+                    }
+                    unorderedSet.Add(arr[j]);
+                }
+            }
+        }
+
+        /// <summary>
+        /// O(n^2), reduces space complexity and avoids to use DataStructure
         /// </summary>
         public void PrintTriletSumUsingSortingAnd2Pointers()
         {
@@ -58,39 +93,6 @@ namespace VinPrograms
                         left++;
                     else
                         right--;
-                }
-            }
-        }
-
-        /// <summary>
-        /// O(n^2), can try. But require to update PairSum class methods.
-        /// </summary>
-        public void PrintTriletSumUsingPairSum()
-        {
-            
-        }
-
-        /// <summary>
-        /// O(n^2)
-        /// </summary>
-        public void PrintTriletSumUsingHashSet()
-        {
-            //int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 15 };
-            //int target = 18;
-            int[] arr = { 3,2,1 };
-            int target = 6;
-
-            for (int i = 0; i < arr.Length-2; i++)
-            {
-                HashSet<int> unorderedSet = new HashSet<int>();
-                int targetPairSum = target - arr[i];
-                for (int j = i+1; j < arr.Length; j++)
-                {
-                    if(unorderedSet.Contains(targetPairSum-arr[j]))
-                    {
-                        Console.WriteLine($"Sum of triplet {arr[i]}, {arr[j]}, {targetPairSum - arr[j]} is {target}");
-                    }
-                    unorderedSet.Add(arr[j]);
                 }
             }
         }
