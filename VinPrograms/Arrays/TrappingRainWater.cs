574aac42371b2ba843744cd7d85b2fae6e4e5892
Vinay Kumar <vnetpgmr@gmail.com>
2021-12-04 21:31:20 -0800

    TrappingRainWater

--- VinPrograms/Arrays/TrappingRainWater.cs
+++ VinPrograms/Arrays/TrappingRainWater.cs
@@ -6,10 +6,42 @@ namespace VinPrograms.Arrays
 {
     public class TrappingRainWater
     {
-        public int Trap()
+        public void Trap()
         {
-            int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
-            return 0;
+            int[] heights = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+
+            if (heights.Length < 3)
+                Console.WriteLine("No water trapped");
+
+            int water = 0;
+            int n = heights.Length;
+            int[] leftMax = new int[n];
+            leftMax[0] = heights[0];
+
+            int[] rightMax = new int[n];
+            rightMax[n - 1] = heights[n - 1];
+
+            for (int i = 1; i < n; i++)
+            {
+                int leftMaxValue = Math.Max(heights[i], leftMax[i - 1]);
+                leftMax[i] = leftMaxValue;
+            }
+
+            for (int i = n - 2; i >= 0; i--)
+            {
+                int rightMaxValue = Math.Max(heights[i], rightMax[i + 1]);
+                rightMax[i] = rightMaxValue;
+            }
+
+            // The real water level is min of highest water from the left 
+            // and highest water from the right subtract the building's high (if any) 
+            // from under neath.
+            for (int i = 0; i < n - 1; ++i)
+            {
+                water += Math.Min(leftMax[i], rightMax[i]) - heights[i];
+            }
+
+            Console.WriteLine($"water trapped: {water}");
         }
     }
 }
