5fdfd4e5e7bbd634ad50b16afda63618b639329a
Vinay Kumar <vnetpgmr@gmail.com>
2021-12-12 17:16:48 -0800

    BubbleSort

--- VinPrograms/Sortings/BubbleSort.cs
+++ VinPrograms/Sortings/BubbleSort.cs
@@ -4,35 +4,30 @@ using System.Text;
 
 namespace VinPrograms.Sortings
 {
-    public class SelectionSort
+    /// <summary>
+    /// Time complexity: o(n*n), Space complexity: o(1)
+    /// But best case time complexity is o(n)
+    /// Largest will be sorted &fixed first
+    /// Use case: In each i'th round, i'th largest element will be identified
+    /// A stable algorithm
+    /// </summary>
+    public class BubbleSort
     {
-        /// <summary>
-        /// Time complexity: o(n*n), Space complexity: o(1)
-        /// Smallest will be sorted&fixed first
-        /// Use case: When array size is small
-        /// Not a stable algorithm
-        /// </summary>
-        public void ApplySelectionSort()
+        public void ApplyBubbleSort()
         {
             int[] arr = { 64, 25, 12, 22, 11 };
 
             int i = 0;
-
             while (i < arr.Length - 1)
             {
-                int currentIndex = i + 1;
-                int minimumIndex = i;
-
-                while (currentIndex < arr.Length)
+                int currentIndex = 0;
+                while (currentIndex < arr.Length - 1 - i)
                 {
-                    if (arr[currentIndex] < arr[minimumIndex])
-                    {
-                        minimumIndex = currentIndex;
-                    }
+                    if (arr[currentIndex] > arr[currentIndex + 1])
+                        Swap(arr, currentIndex, currentIndex + 1);
+
                     currentIndex++;
                 }
-
-                Swap(arr, i, minimumIndex);
 
                 i++;
             }
@@ -47,13 +42,6 @@ namespace VinPrograms.Sortings
             arr[a] = temp;
         }
 
-        //private void swap(int a, int b)
-        //{
-        //    int temp = a;
-        //    a = b;
-        //    b = temp;
-        //}
-        
         private static void PraintArray(int[] arr)
         {
             int p = 0;
--- VinPrograms/Sortings/Programs.cs
+++ VinPrograms/Sortings/Programs.cs
@@ -8,8 +8,15 @@ namespace VinPrograms.Sortings
     {
         static void Main(string[] args)
         {
-            SelectionSort ss = new SelectionSort();
-            ss.ApplySelectionSort();
+            // Stable: Stable sorting algorithms maintain the relative order of records with equal keys (i.e. values). 
+            //That is, a sorting algorithm is stable if whenever there are two records R and S with the same key and 
+            //with R appearing before S in the original list,R will appear before S in the sorted list.
+
+            //SelectionSort ss = new SelectionSort();
+            //ss.ApplySelectionSort();
+
+            BubbleSort bs = new BubbleSort();
+            bs.ApplyBubbleSort();
         }
     }
 }
