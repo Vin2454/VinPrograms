9173db6334feb487008b81db8100634dc921371b
Vinay Kumar <vnetpgmr@gmail.com>
2021-12-21 16:07:11 -0800

    SegmentedSieve

---
+++ VinPrograms/Arrays/SegmentedSieve.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VinPrograms.Arrays
+{
+    /// <summary>
+    /// https://www.geeksforgeeks.org/segmented-sieve/
+    /// </summary>
+    public class SegmentedSieve
+    {
+    }
+}
