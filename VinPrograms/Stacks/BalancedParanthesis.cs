57dfb3ebb467e7a2c3fea43b6d0752478d06aac6
Vinay Kumar <vnetpgmr@gmail.com>
2022-01-02 20:18:54 -0800

    BalancedParanthesis

---
+++ VinPrograms/Stacks/BalancedParanthesis.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VinPrograms.Stacks
+{
+    public class BalancedParanthesis
+    {
+        public void BalancedParanthesisCheck()
+        {
+            string input = "{(a+b+c)+[d]}";
+            Console.WriteLine(IsBalancedParanthesis(input));
+        }
+
+        public bool IsBalancedParanthesis(string input)
+        {
+            int start = 0;
+            int end = input.Length;
+            Stack<char> paranethesis = new Stack<char>();
+            while (start < end)
+            {
+                char ch = input[start];
+                switch (ch)
+                {
+                    case '{':
+                    case '(':
+                    case '[':
+                        paranethesis.Push(ch);
+                        break;
+
+                    case '}':
+                        if (paranethesis.Count != 0 && paranethesis.Peek() == '{')
+                        {
+                            paranethesis.Pop();
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                        break;
+                    case ')':
+                        if (paranethesis.Count != 0 && paranethesis.Peek() == '(')
+                        {
+                            paranethesis.Pop();
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                        break;
+                    case ']':
+                        if (paranethesis.Count != 0 && paranethesis.Peek() == '[')
+                        {
+                            paranethesis.Pop();
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                start++;
+            }
+
+            if (paranethesis.Count == 0)
+                return true;
+
+            return false;
+        }
+    }
+}
--- VinPrograms/Stacks/Program.cs
+++ VinPrograms/Stacks/Program.cs
@@ -6,6 +6,9 @@ namespace VinPrograms.Stacks
     {
         static void Main(string[] args)
         {
+            //BalancedParanthesis bpc = new BalancedParanthesis();
+            //bpc.BalancedParanthesisCheck();
+
             //NearestGreaterElementOnTheRightInArray ngeor = new NearestGreaterElementOnTheRightInArray();
             ////ngeor.PrintNextGreaterElementOnTheRightInArray_Solution1();
             //ngeor.PrintNextGreaterElementOnTheRightInArray_BetterSolution();
@@ -18,6 +21,16 @@ namespace VinPrograms.Stacks
 
             //NearestSmallerElementOnTheRightInArray nseor = new NearestSmallerElementOnTheRightInArray();
             //nseor.PrintNextSmallerElementOnTheRightInArray_BetterSolution();
+
+            // Check Stock Span Problem: https://www.youtube.com/watch?v=p9T-fE1g1pU&list=PL_z_8CaSLPWdeOezg68SKkeLN4-T_jNHd&index=7
+
+            // Check Largest Rectangular Area or Maximum Area Histogram : https://www.geeksforgeeks.org/largest-rectangle-under-histogram/
+            // https://www.youtube.com/watch?v=J2X70jj_I1o&list=PL_z_8CaSLPWdeOezg68SKkeLN4-T_jNHd&index=7
+
+            // Check Max Area Rectangle in binary matrix: https://www.youtube.com/watch?v=St0Jf_VmG_g&list=PL_z_8CaSLPWdeOezg68SKkeLN4-T_jNHd&index=8
+
+            // Minimum Element in Stack in O(1) Space: https://www.youtube.com/watch?v=ZvaRHYYI0-4&list=PL_z_8CaSLPWdeOezg68SKkeLN4-T_jNHd&index=11
+            // Check comments section to understand why to use "2 * ME - Y" 
         }
     }
 }
